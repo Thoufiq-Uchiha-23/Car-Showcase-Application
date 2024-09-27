c8811f4365f9e2d72f09832ca3189d1300fafe7c
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-09-27 23:21:29 +0530

    page of app

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,28 @@
 import Image from "next/image";
-import { Hero } from "@/components";
+import { ContainerFilter, Hero, SearchBar } from "@/components";
 
 export default function Home() {
   return (
-      <main className="overflow-hidden">
-        <Hero />
-      </main>
+    <main className="overflow-hidden">
+      <Hero />
+
+      {/* Car Catalogue Section */}
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+          <p>Explore the cars yo might like</p>
+        </div>
+
+        {/* Filters */}
+        <div className="home__filters">
+          <SearchBar />
+
+          <div className="home__filter-container">
+            <ContainerFilter title="fuel" />
+            <ContainerFilter title="year" />
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
