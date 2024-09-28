2d6e3b2b96a2d530f68a525ac659a4304f22039a
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-09-28 21:12:51 +0530

    Search Manufacturer

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,51 +1,88 @@
-"use client"
+"use client";
 
-import { manufacturers } from '@/constants'
-import { SearchManufacturerProps } from '@/types'
-import { Combobox, Transition, ComboboxButton, ComboboxInput } from '@headlessui/react'
-import Image from 'next/image'
-import { useState, Fragment } from 'react'
+import { manufacturers } from "@/constants";
+import { SearchManufacturerProps } from "@/types";
+import {
+  Combobox,
+  Transition,
+  ComboboxButton,
+  ComboboxInput,
+  ComboboxOptions,
+  ComboboxOption,
+} from "@headlessui/react";
+import Image from "next/image";
+import { useState, Fragment } from "react";
 
-const SearchManufacturer = ({manufacturer, setManufacturer}:SearchManufacturerProps) => {
-  const [query, setQuery] = useState('')
+const SearchManufacturer = ({
+  manufacturer,
+  setManufacturer,
+}: SearchManufacturerProps) => {
+  const [query, setQuery] = useState("");
 
-  // const filteredManufacturers = query === "" ? manufacturers : manufacturers.filter((item) => ())
+  const filteredManufacturers =
+    query === ""
+      ? manufacturers
+      : manufacturers.filter((item) =>
+          item
+            .toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.toLowerCase().replace(/\s+/g, ""))
+        );
 
   return (
+    <div className="search-manufacturer">
+      <Combobox>
+        <div className="relative w-full">
+          <ComboboxButton className="absolute top-[14px]">
+            <Image
+              src="/car-logo.svg"
+              width={20}
+              height={20}
+              className="ml-4"
+              alt="Car Logo"
+            />
+          </ComboboxButton>
 
-    <div className='search-manufacturer'>
-        <Combobox>
-            <div className='relative w-full'>
-                <ComboboxButton className="absolute top-[14px]" >
-                  <Image
-                    src="/car-logo.svg"
-                    width={20}
-                    height={20}
-                    className='ml-4'
-                    alt='Car Logo'
-                  />
-                </ComboboxButton>
+          <ComboboxInput
+            className="search-manufacturer__input"
+            placeholder="Volkswagen"
+            displayValue={(manufacturer: string) => manufacturer}
+            onChange={(e) => setQuery(e.target.value)}
+          />
 
-                <ComboboxInput 
-                  className="search-manufacturer__input"
-                  placeholder='Volkswagen'
-                  displayValue={(manufacturer:string) => (manufacturer)}
-                  onChange={(e) => setQuery(e.target.value)}
-                />
-
-                <Transition
-                  as={Fragment}
-                  leave='transition ease-in duration-100'
-                  leaveFrom='opacity-100'
-                  leaveTo='opacity-0'
-                  afterLeave={() => setQuery('')}
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => setQuery("")}
+          >
+            <ComboboxOptions>
+              {filteredManufacturers.map((item) => (
+                <ComboboxOption
+                  key={item}
+                  className={({ focus }) =>
+                    `relative search-manufacturer__option ${
+                      focus
+                        ? "bg-primary-blue-100 text-white"
+                        : "text-gray-900"
+                    }`
+                  }
+                  value={item}
                 >
-                  
-                </Transition>
-            </div>
-        </Combobox>
+                  {({selected, active}) => {
+                    <>
+                    
+                    </>
+                  }}
+                </ComboboxOption>
+              ))}
+            </ComboboxOptions>
+          </Transition>
+        </div>
+      </Combobox>
     </div>
-  )
-}
+  );
+};
 
-export default SearchManufacturer
+export default SearchManufacturer;
