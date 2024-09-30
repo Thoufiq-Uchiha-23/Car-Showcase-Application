aea430da3a985006a17849cb7d72faba8fcfcf04
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-09-30 23:50:46 +0530

    components index updated

--- components/index.tsx
+++ components/index.tsx
@@ -1,7 +1,7 @@
 import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import SearchBar from "./SearchBar";
-import ContainerFilter from "./ContainerFilter";
+import ContainerFilter from "./CustomFilter";
 import SearchManufacturer from "./SearchManufacturer";
 
 export { Hero, CustomButton, SearchBar, ContainerFilter, SearchManufacturer };
