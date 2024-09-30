f7d724f19c55cb95a6808349efbcf0a4d0addee9
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-09-30 23:03:29 +0530

    Mission: SearchManufacturer Component Completed

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { manufacturers } from "@/constants";
-import { SearchManufacturerProps } from "@/types";
+import { SearchManuFacturerProps } from "@/types";
 import {
   Combobox,
   Transition,
@@ -9,16 +9,13 @@ import {
   ComboboxInput,
   ComboboxOptions,
   ComboboxOption,
-  CheckIcon
 } from "@headlessui/react";
+import { CheckIcon, ChevronDownIcon } from '@heroicons/react/20/solid'
+import clsx from 'clsx'
 import Image from "next/image";
 import { useState, Fragment } from "react";
 
-const SearchManufacturer = ({
-  manufacturer,
-  setManufacturer,
-}: SearchManufacturerProps) => {
-  const [selectedPerson, setSelectedPerson] = useState([0])
+const SearchManufacturer = ({ manufacturer, setManuFacturer }: SearchManuFacturerProps) => {
   const [query, setQuery] = useState("");
 
   const filteredManufacturers =
@@ -32,55 +29,75 @@ const SearchManufacturer = ({
         );
 
   return (
-    <div className="search-manufacturer">
-      <Combobox>
-        <div className="relative w-full">
-          <ComboboxButton className="absolute top-[14px]">
+    <div className='search-manufacturer'>
+      <Combobox value={manufacturer} onChange={setManuFacturer}>
+        <div className='relative w-full'>
+          {/* Button for the combobox. Click on the icon to see the complete dropdown */}
+          <Combobox.Button className='absolute top-[14px]'>
             <Image
-              src="/car-logo.svg"
+              src='/car-logo.svg'
               width={20}
               height={20}
-              className="ml-4"
-              alt="Car Logo"
+              className='ml-4'
+              alt='car logo'
             />
-          </ComboboxButton>
+          </Combobox.Button>
 
-          <ComboboxInput
-            className="search-manufacturer__input"
-            placeholder="Volkswagen"
-            displayValue={(manufacturer: string) => manufacturer}
-            onChange={(e) => setQuery(e.target.value)}
+          {/* Input field for searching */}
+          <Combobox.Input
+            className='search-manufacturer__input'
+            displayValue={(item: string) => item}
+            onChange={(event) => setQuery(event.target.value)} // Update the search query when the input changes
+            placeholder='Volkswagen...'
           />
 
+          {/* Transition for displaying the options */}
           <Transition
-            as={Fragment}
-            leave="transition ease-in duration-100"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-            afterLeave={() => setQuery("")}
+            as={Fragment} // group multiple elements without introducing an additional DOM node i.e., <></>
+            leave='transition ease-in duration-100'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
+            afterLeave={() => setQuery("")} // Reset the search query after the transition completes
           >
-            <ComboboxOptions>
-              {filteredManufacturers.map(() => (
-                <ComboboxOption
-                  as={Fragment}
-                  key={""}
-                  value={""}
-                  // className="data-[focus]:bg-blue-100"
+            <Combobox.Options
+              className='absolute mt-1 max-h-auto w-full rounded-md border-[1px] border-solid border-[#4D74FF] py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'
+              static
+            >
+              {filteredManufacturers.length === 0 && query !== "" ? (
+                <Combobox.Option
+                  value={query}
+                  className='search-manufacturer__option'
                 >
-                  {({ focus, selected }) => (
-                    <div
-                      className={clsx(
-                        "group flex gap-2",
-                        focus && "bg-blue-100"
-                      )}
-                    >
-                      {selected && <CheckIcon className="size-5" />}
-                      {""}
-                    </div>
-                  )}
-                </ComboboxOption>
-              ))}
-            </ComboboxOptions>
+                  Create "{query}"
+                </Combobox.Option>
+              ) : (
+                filteredManufacturers.map((item) => (
+                  <Combobox.Option
+                    key={item}
+                    className={({ active }) =>
+                      `relative search-manufacturer__option ${
+                        active ? "bg-primary-blue text-white" : "text-gray-900"
+                      }`
+                    }
+                    value={item}
+                  >
+                    {({ selected, active }) => (
+                      <div>
+                        <span className={`block truncate ${selected ? "font-medium" : "font-normal"}`}>
+                          {item}
+                        </span>
+
+                        {/* Show an active blue background color if the option is selected */}
+                        {selected ? (
+                          <span className={`absolute inset-y-0 left-0 flex items-center pl-3 ${active? "text-white": "text-pribg-primary-purple"}`}
+                          ></span>
+                        ) : null}
+                      </div>
+                    )}
+                  </Combobox.Option>
+                ))
+              )}
+            </Combobox.Options>
           </Transition>
         </div>
       </Combobox>
