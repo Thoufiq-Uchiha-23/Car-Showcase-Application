1ee13163008b3930de618681179f9bfb98abdbaf
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-10-01 17:17:30 +0530

    components index updated

--- components/index.tsx
+++ components/index.tsx
@@ -3,5 +3,7 @@ import CustomButton from "./CustomButton";
 import SearchBar from "./SearchBar";
 import ContainerFilter from "./CustomFilter";
 import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
 
-export { Hero, CustomButton, SearchBar, ContainerFilter, SearchManufacturer };
+export { Hero, CustomButton, SearchBar, ContainerFilter, SearchManufacturer, CarCard, CarDetails };
