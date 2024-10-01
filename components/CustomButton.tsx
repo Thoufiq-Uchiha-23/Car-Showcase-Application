92c76c1423d6f8b70a198ca20748ee396740f47f
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-10-01 19:51:55 +0530

    prettier

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,26 +1,33 @@
-"use client"
-import { CustomButtonProps } from "@/types"
-import Image from "next/image"
+"use client";
+import { CustomButtonProps } from "@/types";
+import Image from "next/image";
 
-const CustomButton = ({title, containerStyles, btnType, textStyles,rightIcon, handleClick}: CustomButtonProps) => {
+const CustomButton = ({
+  title,
+  containerStyles,
+  btnType,
+  textStyles,
+  rightIcon,
+  handleClick,
+}: CustomButtonProps) => {
   return (
-    <button 
-    disabled={false}
-    type={btnType || "button"}
-    className={`custom-btn ${containerStyles}`}
-    onClick={handleClick}
+    <button
+      disabled={false}
+      type={btnType || "button"}
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
     >
-        <span className={`flex-1 ${textStyles}`}>{title}</span>
-        {rightIcon && (
-          <Image 
-            src={rightIcon}
-            alt="right icon"
-            fill
-            className="object-contain"
-          />
-        )}
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+      {rightIcon && (
+        <Image
+          src={rightIcon}
+          alt="right icon"
+          fill
+          className="object-contain"
+        />
+      )}
     </button>
-  )
-}
+  );
+};
 
-export default CustomButton
+export default CustomButton;
