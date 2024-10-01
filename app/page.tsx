9d0b49a67e60cf92fafa8d531773be627ed819c8
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-10-01 17:15:54 +0530

    adding card section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import { ContainerFilter, Hero, SearchBar } from "@/components";
+import { CarCard, ContainerFilter, Hero, SearchBar } from "@/components";
 import { fetchCars } from "@/utils";
 
 export default async function Home() {
-  const allCars = await fetchCars()
-   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
-  
+  const allCars = await fetchCars();
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
     <main className="overflow-hidden">
       <Hero />
@@ -29,13 +29,18 @@ export default async function Home() {
 
         {!isDataEmpty ? (
           <section>
-            WE HAVE CARS
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
           </section>
-        ) : <div className="home__error-container">
-              <h2 className="text-black text-xl font-bold">Oops, no results</h2>
-              <p>{allCars?.message}</p>
-        </div> }
-
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">Oops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   );
